447ae6bbc8fa89a815eb88ac9ec72f53574cd3c5
Mitch Souders <mitch.souders@gmail.com>
2015-11-22 16:30:49 -0800

    Restructure server and base IRC class for extensibility

--- src/IRC.py
+++ src/IRC.py
@@ -1,5 +1,8 @@
-from jsonschema import validate
+import jsonschema
 import json
+from abc import ABCMeta, abstractmethod
+import signal
+import select
 
 IRCNick = '[a-zA-Z0-9]{1,10}'
 IRCChannel = '#[a-zA-Z0-9]{1,10}'
@@ -100,8 +103,9 @@ IRCSchema = {
                     'minItems':1,
                     'uniqueItems': True
                 },
+                'msg': {'type':'string'}
             },
-            'required':['channels']
+            'required':['channels', 'msg']
         },
         'channels':{            
             'type': 'object',
@@ -216,86 +220,215 @@ IRCSchema = {
         }
     }
 }
-
-def ValidateIRC(f):
-    def validate_irc_msg(*args, **kwargs):
-        msg = f(*args, **kwargs)
-        validate(msg, IRCSchema)
-        jmsg = json.dumps(msg, separators=(',', ':'))
-        if len(jmsg) > 1022:
-            raise "JSON IRC Message Too Long"
-        return jmsg + "\r\n"
     
-    return validate_irc_msg
-    
-class IRC:
+class IRCMessage:
     def __init__(self, src):
         self.__src = src
 
     def updateSrc(self, src):
         self.__src = src
         
-    @ValidateIRC
     def cmdNick(self, nick):
         return {'cmd':'nick', 'src':self.__src, 'update':nick}
     
-    @ValidateIRC
     def cmdQuit(self, msg):
         return {'cmd':'quit', 'src':self.__src, 'msg':msg}
 
-    @ValidateIRC
     def cmdSQuit(self, msg):
         return {'cmd':'squit', 'src':self.__src, 'msg':msg}
 
-    @ValidateIRC
     def cmdJoin(self, channels):
         return {'cmd':'join', 'src':self.__src, 'channels':channels}
 
-    @ValidateIRC
     def cmdLeave(self, channels, msg):
         return {'cmd':'leave', 'src':self.__src, 'channels':channels, 'msg':msg}
 
-    @ValidateIRC
     def cmdChannels(self):
         return {'cmd':'channels', 'src':self.__src}
 
-    @ValidateIRC
     def cmdUsers(self, channels):
         return {'cmd':'users', 'src':self.__src, 'channels':channels}
 
-    @ValidateIRC
     def cmdMsg(self, msg, targets):
         return {'cmd':'msg', 'src':self.__src, 'msg':msg, 'targets':targets}
 
-    @ValidateIRC
     def cmdPing(self, msg):
         return {'cmd':'ping', 'src':self.__src, 'msg':msg}
 
-    @ValidateIRC
     def cmdPong(self, msg):
         return {'cmd':'pong', 'src':self.__src, 'msg':msg}
 
-    @ValidateIRC
     def errorMsg(self, type, msg):
         return {'error':type, 'msg':msg}
 
-    @ValidateIRC
     def replyOk(self):
         return {'reply':'ok'}
 
-    @ValidateIRC
     def replyNames(self, names):
         return {'reply':'names', 'names':names}
-    
-    def processMsg(self, cb, msg):
-        jmsg = json.loads(msg)
-        validate(jmsg, IRCSchema)
-        msg_type = 'unknown'
-        if 'cmd' in msg:
-            msg_type = 'cmd'
-        elif 'reply' in msg:
-            msg_type = 'reply'
-        elif 'error' in msg:
-            msg_type = 'error'
 
+class InvalidIRCMessage(Exception):
+    def __init__(self, socket, msg):
+        self.socket = socket
+        self.msg = msg
+        
+class IRCHandler():
+    __metaclass__ = ABCMeta
+    def __init__(self, src):
+        self._ircmsg = IRCMessage(src)
+        self.__running = True
+        self.__timeout = 0.1
+        self.__handlers = {
+            'cmd':{
+                'nick'     : lambda s, msg: self.receivedNick(s, msg['update']),
+                'quit'     : lambda s, msg: self.receivedQuit(s, msg['msg']),
+                'squit'    : lambda s, msg: self.receivedSQuit(s, msg['msg']),
+                'join'     : lambda s, msg: self.receivedJoin(s, msg['channels']),
+                'leave'    : lambda s, msg: self.receivedLeave(s, msg['channels'], msg['msg']),
+                'channels' : lambda s, msg: self.receivedChannels(s),
+                'users'    : lambda s, msg: self.receivedUsers(s),
+                'ping'     : lambda s, msg: self.receivedPing(s, msg['msg']),
+                'pong'     : lambda s, msg: self.receivedPong(s, msg['msg']),
+                'msg'      : lambda s, msg: self.receivedMsg(s, msg['targets'], msg['msg']),            
+            },
+            'reply':{
+                'ok'   : lambda s, msg: self.receivedOk(s),
+                'names': lambda s, msg: self.receivedNames(s, msg['names'])
+            },
+            'error':{
+                'error' : lambda s, msg: self.receivedError(s, msg['error'], msg['msg'])
+            }
+        }
+        signal.signal(signal.SIGINT, self.receivedSignal)
+    
+    def run(self):
+        while self.__running:
+            try:
+                inputready, outputready, exceptready = select.select(self.getSocketList(),[],[],self.__timeout)
+                for s in inputready:
+                    self.socketInputReady(s)                    
+                for s in exceptready:
+                    self.socketExceptReady(s)
+            except InvalidIRCMessage as e:
+                self.sentInvalid(e.socket, e.msg)            
+        
+    def sendMsg(self, socket, msg):
+        try:
+            jsonschema.validate(msg, IRCSchema)
+            jmsg = json.dumps(msg, separators=(',', ':')) + "\r\n"
+            if len(jmsg) > 1024:
+                raise "JSON IRC Message Too Long"
+        except jsonschema.exceptions.ValidationError:            
+            raise InvalidIRCMessage(socket, msg)
+        else:
+            socket.send(jmsg)
+
+    def processIRCMsg(self, socket, msg):
+        try:
+            jmsg = json.loads(msg)
+        except ValueError as e:
+            self.receivedInvalid(socket, msg)
+            return
+        
+        try:
+            jsonschema.validate(jmsg, IRCSchema)
+        except jsonschema.exceptions.ValidationError as e:
+            self.receivedInvalid(socket, msg)
+            return
+        
+        if 'cmd' in jmsg:
+            self.__handlers['cmd'][jmsg['cmd']](socket, jmsg)
+        elif 'reply' in jmsg:
+            self.__handlers['reply'][jmsg['reply']](socket, jmsg)
+        elif 'error' in jmsg:
+            self.__handlers['error']['error'](socket, jmsg)
+        else:
+            raise BaseException("Unhandled Message Type")
+            self.receivedInvalid(socket, msg) #how did we get here?
             
+    def receiveMsg(self, socket):
+        msg = socket.recv(1024)
+        if msg == '':
+            self.connectionDrop(socket)
+        else:
+            self.processIRCMsg(socket, msg)
+                          
+    @abstractmethod
+    def getSocketList(self):
+        pass
+
+    @abstractmethod
+    def socketInputReady(self, socket):
+        pass
+
+    @abstractmethod
+    def socketExceptReady(self, socket):
+        pass
+    
+    @abstractmethod
+    def connectionDrop(self, socket):
+        pass
+
+    @abstractmethod
+    def receivedNick(self, socket, newnick):
+        pass
+
+    @abstractmethod
+    def receivedQuit(self, socket, msg):
+        pass
+
+    @abstractmethod
+    def receivedSQuit(self, socket, msg):
+        pass
+
+    @abstractmethod
+    def receivedJoin(self, socket, channels):
+        pass
+
+    @abstractmethod
+    def receivedLeave(self, socket, channels, msg):
+        pass
+
+    @abstractmethod
+    def receivedChannels(self, socket):
+        pass
+
+    @abstractmethod
+    def receivedUsers(self, socket, channels):
+        pass
+
+    @abstractmethod
+    def receivedMsg(self, socket, msg):
+        pass
+
+    @abstractmethod
+    def receivedPing(self, socket, msg):
+        pass
+
+    @abstractmethod
+    def receivedPong(self, socket, msg):
+        pass
+
+    @abstractmethod
+    def receivedOk(self, socket):
+        pass
+
+    @abstractmethod
+    def receivedNames(self, socket, names):
+        pass
+
+    @abstractmethod
+    def receivedError(self, socket, error_name, error_msg):
+        pass
+        
+    @abstractmethod    
+    def receivedInvalid(self, socket, msg):
+        pass
+
+    @abstractmethod
+    def receivedSignal(self, signal, frame):
+        pass
+
+    @abstractmethod
+    def sentInvalid(self, socket, msg):
+        pass
