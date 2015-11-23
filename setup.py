02d1778d9f232a8a0296c11bfbb831e0a8caaa1d
Mitch Souders <mitch.souders@gmail.com>
2015-11-23 15:02:59 -0800

    Update Setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='IRC594',
+      version='1.0',
+      description='IRC Client For CS594',
+      author='Mitch Souders',
+      author_email='msouders@pdx.edu',
+      scripts=[
+          'src/irc_server',
+          'src/irc_client',          
+      ],
+      package_dir={'':'src'},
+      py_modules=[
+          'IRC'
+      ],
+      #url='',
+      #packages=['distutils', 'distutils.command'],
+      setup_requires=[
+          'petname',
+          'jsonschema',
+          'more_itertools',
+      ]
+  )
